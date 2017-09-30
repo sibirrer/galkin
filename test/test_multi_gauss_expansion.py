a67e23ec9e712ce2e4def2370594e45f75bfbcbe
Simon Birrer <simon.birrer@pyhs.ethz.ch>
2017-09-29 22:07:56 -0700

    test of Hernquist assumption relative to Hernquist model

--- test/test_multi_gauss_expansion.py
+++ test/test_multi_gauss_expansion.py
@@ -7,6 +7,7 @@ import astrofunc.multi_gauss_expansion as mge
 import numpy as np
 from lenstronomy.ImSim.light_model import LightModel
 from lenstronomy.ImSim.lens_model import LensModel
+from lenstronomy.LensAnalysis.lens_analysis import LensAnalysis
 from galkin.galkin import Galkin
 
 
@@ -96,7 +97,7 @@ class TestGalkin(object):
 
         # mge of lens profile
         lensModel = LensModel(kwargs_options={'lens_model_list': mass_profile_list})
-        r_array = np.logspace(-2, 2, 100)
+        r_array = np.logspace(-2, 1, 100)*theta_E
         kappa_r = lensModel.kappa(r_array, 0, kwargs_profile)
         amps, sigmas, norm = mge.mge_1d(r_array, kappa_r, N=20)
         mass_profile_list_mge = ['MULTI_GAUSSIAN_KAPPA']
@@ -167,6 +168,66 @@ class TestGalkin(object):
         print (sigma_v/sigma_v2)**2
 
         npt.assert_almost_equal((sigma_v-sigma_v2)/sigma_v2, 0, decimal=2)
+
+    def test_sersic_vs_hernquist_kinematics(self):
+        """
+        attention: this test only works for Sersic indices > \approx 2!
+        Lower n_sersic will result in different predictions with the Hernquist assumptions
+        replacing the correct Light model!
+        :return:
+        """
+        # anisotropy profile
+        anisotropy_type = 'OsipkovMerritt'
+        r_ani = 2.
+        kwargs_anisotropy = {'r_ani': r_ani}  # anisotropy radius [arcsec]
+
+        # aperture as slit
+        aperture_type = 'slit'
+        length = 3.8
+        width = 0.9
+        kwargs_aperture = {'length': length, 'width': width, 'center_ra': 0, 'center_dec': 0, 'angle': 0}
+
+        psf_fwhm = 0.7  # Gaussian FWHM psf
+        kwargs_cosmo = {'D_d': 1000, 'D_s': 1500, 'D_ds': 800}
+
+        # light profile
+        light_profile_list = ['SERSIC']
+        r_sersic = 1.5
+        n_sersic = 3.
+        kwargs_light = [{'I0_sersic': 1., 'R_sersic':  r_sersic, 'n_sersic': n_sersic}]  # effective half light radius (2d projected) in arcsec
+
+        # mass profile
+        mass_profile_list = ['SPP']
+        theta_E = 1.2
+        gamma = 2.
+        kwargs_profile = [{'theta_E': theta_E, 'gamma': gamma}]  # Einstein radius (arcsec) and power-law slope
+
+        # mge of light profile
+        lightModel = LightModel(light_profile_list)
+        r_array = np.logspace(-2, 2, 100)
+        flux_r = lightModel.surface_brightness(r_array, 0, kwargs_light)
+        amps, sigmas, norm = mge.mge_1d(r_array, flux_r, N=20)
+        light_profile_list_mge = ['MULTI_GAUSSIAN']
+        kwargs_light_mge = [{'amp': amps, 'sigma': sigmas}]
+
+        # Hernquist fit to Sersic profile
+        lens_analysis = LensAnalysis({'lens_light_model_list': ['SERSIC'], 'lens_model_list': ['NONE']}, {})
+        r_eff = lens_analysis.half_light_radius(kwargs_light)
+        print(r_eff)
+        light_profile_list_hernquist = ['HERNQUIST']
+        kwargs_light_hernquist = [{'Rs': r_eff*0.551, 'sigma0': 1.}]
+
+        galkin = Galkin(mass_profile_list, light_profile_list_hernquist, aperture_type=aperture_type, anisotropy_model=anisotropy_type, fwhm=psf_fwhm, kwargs_cosmo=kwargs_cosmo)
+        sigma_v = galkin.vel_disp(kwargs_profile, kwargs_light_hernquist, kwargs_anisotropy, kwargs_aperture, num=1000)
+
+        galkin = Galkin(mass_profile_list, light_profile_list_mge, aperture_type=aperture_type, anisotropy_model=anisotropy_type, fwhm=psf_fwhm, kwargs_cosmo=kwargs_cosmo)
+        sigma_v2 = galkin.vel_disp(kwargs_profile, kwargs_light_mge, kwargs_anisotropy, kwargs_aperture, num=1000)
+
+        print sigma_v, sigma_v2, 'sigma_v Galkin, sigma_v MGEn'
+        print (sigma_v/sigma_v2)**2
+
+        npt.assert_almost_equal((sigma_v-sigma_v2)/sigma_v2, 0, decimal=2)
+
 
 if __name__ == '__main__':
     pytest.main()
