numerical stability and fix in logarithmic integral. All test match a sub-precentage accuracy relative to the analytic results, for the specific cases.

galkin/galkin.py
@@ -14,15 +14,19 @@ class Galkin(object):
     major class to compute velocity dispersion measurements given light and mass models
     """
     def __init__(self, mass_profile_list, light_profile_list, aperture_type='slit', anisotropy_model='isotropic',
-                 fwhm=0.7, kwargs_cosmo={'D_d': 1000, 'D_s': 2000, 'D_ds': 500}):
+                 fwhm=0.7, kwargs_numerics={}, kwargs_cosmo={'D_d': 1000, 'D_s': 2000, 'D_ds': 500}):
         self.massProfile = MassProfile(mass_profile_list, kwargs_cosmo)
         self.lightProfile = LightProfile(light_profile_list)
         self.aperture = Aperture(aperture_type)
         self.anisotropy = MamonLokasAnisotropy(anisotropy_model)
         self.FWHM = fwhm
         self.cosmo = Cosmo(kwargs_cosmo)
+        self._num_sampling = kwargs_numerics.get('sampling_number', 1000)
+        self._interp_grid_num = kwargs_numerics.get('interpol_grid_num', 1000)
+        self._log_int = kwargs_numerics.get('log_integration', False)
+        self._max_integrate = kwargs_numerics.get('max_integrate', 100)  # maximal integration (and interpolation) in units of arcsecs
 
-    def vel_disp(self, kwargs_mass, kwargs_light, kwargs_anisotropy, kwargs_apertur, num=1000, r_eff=1., grid_num=100, log_int=False):
+    def vel_disp(self, kwargs_mass, kwargs_light, kwargs_anisotropy, kwargs_apertur, r_eff=1.):
         """
         computes the averaged LOS velocity dispersion in the slit (convolved)
         :param gamma:
@@ -34,15 +38,15 @@ class Galkin(object):
         :return:
         """
         sigma2_R_sum = 0
-        for i in range(0, num):
-            sigma2_R = self.draw_one_sigma2(kwargs_mass, kwargs_light, kwargs_anisotropy, kwargs_apertur, r_eff=r_eff, num=grid_num, log_int=log_int)
+        for i in range(0, self._num_sampling):
+            sigma2_R = self.draw_one_sigma2(kwargs_mass, kwargs_light, kwargs_anisotropy, kwargs_apertur, r_eff=r_eff)
             sigma2_R_sum += sigma2_R
-        sigma_s2_average = sigma2_R_sum / num
+        sigma_s2_average = sigma2_R_sum / self._num_sampling
         # apply unit conversion from arc seconds and deflections to physical velocity disperison in (km/s)
         sigma_s2_average *= 2 * const.G  # correcting for integral prefactor
         return np.sqrt(sigma_s2_average/(const.arcsec**2 * self.cosmo.D_d**2 * const.Mpc))/1000.  # in units of km/s
 
-    def draw_one_sigma2(self, kwargs_mass, kwargs_light, kwargs_anisotropy, kwargs_aperture, r_eff=1., num=1000, log_int=False):
+    def draw_one_sigma2(self, kwargs_mass, kwargs_light, kwargs_anisotropy, kwargs_aperture, r_eff=1.):
         """
 
         :param kwargs_mass:
@@ -58,10 +62,10 @@ class Galkin(object):
             bool = self.aperture.aperture_select(x_, y_, kwargs_aperture)
             if bool is True:
                 break
-        sigma2_R = self.sigma2_R(R, kwargs_mass, kwargs_light, kwargs_anisotropy, num=num, log_int=log_int)
+        sigma2_R = self.sigma2_R(R, kwargs_mass, kwargs_light, kwargs_anisotropy)
         return sigma2_R
 
-    def sigma2_R(self, R, kwargs_mass, kwargs_light, kwargs_anisotropy, num=1000, log_int=False):
+    def sigma2_R(self, R, kwargs_mass, kwargs_light, kwargs_anisotropy):
         """
         returns unweighted los velocity dispersion
         :param R:
@@ -70,12 +74,12 @@ class Galkin(object):
         :param kwargs_anisotropy:
         :return:
         """
-        I_R_sigma2 = self.I_R_simga2(R, kwargs_mass, kwargs_light, kwargs_anisotropy, num=num, log_int=log_int)
+        I_R_sigma2 = self.I_R_simga2(R, kwargs_mass, kwargs_light, kwargs_anisotropy)
         I_R = self.lightProfile.light_2d(R, kwargs_light)
         #I_R = self.lightProfile._integrand_light(R, kwargs_light)
         return I_R_sigma2 / I_R
 
-    def I_R_simga2(self, R, kwargs_mass, kwargs_light, kwargs_anisotropy, num=1000, log_int=False):
+    def I_R_simga2(self, R, kwargs_mass, kwargs_light, kwargs_anisotropy):
         """
         equation A15 in Mamon&Lokas 2005 as a logarithmic numerical integral
         modulo pre-factor 2*G
@@ -85,20 +89,20 @@ class Galkin(object):
         :param kwargs_anisotropy:
         :return:
         """
-        if log_int is True:
-            min_log = np.log10(R+0.00001)
-            max_log = np.log10(20)
-            r_array = np.logspace(min_log, max_log, num)
-            dlog_r = (np.log10(r_array[1]) - np.log10(r_array[0]))
-            IR_sigma2_dr = self._integrand_A15(r_array, R, kwargs_mass, kwargs_light, kwargs_anisotropy, log_int) * dlog_r * r_array
+        if self._log_int is True:
+            min_log = np.log10(R+0.0001)
+            max_log = np.log10(self._max_integrate)
+            r_array = np.logspace(min_log, max_log, self._interp_grid_num)
+            dlog_r = (np.log10(r_array[1]) - np.log10(r_array[0])) * np.log(10)
+            IR_sigma2_dr = self._integrand_A15(r_array, R, kwargs_mass, kwargs_light, kwargs_anisotropy) * dlog_r * r_array
         else:
-            r_array = np.linspace(R+0.000001, 20, num)
+            r_array = np.linspace(R + 0.0001, self._max_integrate, self._interp_grid_num)
             dr = r_array[1] - r_array[0]
             IR_sigma2_dr = self._integrand_A15(r_array, R, kwargs_mass, kwargs_light, kwargs_anisotropy) * dr
         IR_sigma2 = np.sum(IR_sigma2_dr)
         return IR_sigma2
 
-    def _integrand_A15(self, r, R, kwargs_mass, kwargs_light, kwargs_anisotropy, log_int=False):
+    def _integrand_A15(self, r, R, kwargs_mass, kwargs_light, kwargs_anisotropy):
         """
         integrand of A15 (in log space)
         :param r:
@@ -110,8 +114,5 @@ class Galkin(object):
         k_r = self.anisotropy.K(r, R, kwargs_anisotropy)
         l_r = self.lightProfile.light_3d_interp(r, kwargs_light)
         m_r = self.massProfile.mass_3d_interp(r, kwargs_mass)
-        if log_int:
-            out = k_r * l_r * m_r
-        else:
-            out = k_r * l_r * m_r / r
+        out = k_r * l_r * m_r / r
         return out

galkin/light_profile.py
@@ -8,12 +8,15 @@ class LightProfile(object):
     """
     class to deal with the light distribution
     """
-    def __init__(self, profile_list=['HERNQUIST']):
+    def __init__(self, profile_list=['HERNQUIST'], kwargs_numerics={}):
         """
 
         :param profile_list:
         """
         self.light_model = LightModel(profile_type_list=profile_list)
+        self._interp_grid_num = kwargs_numerics.get('interpol_grid_num', 1000)
+        self._max_interpolate = kwargs_numerics.get('max_interpolate', 100)
+        self._min_interpolate = kwargs_numerics.get('min_interpolate', 0.0001)
 
     def light_3d(self, r, kwargs_list):
         """
@@ -30,10 +33,9 @@ class LightProfile(object):
         :param kwargs_list:
         :return:
         """
-        if not hasattr(self, '_log_light_3d') or new_compute is True:
-            r_array = np.linspace(0.0001, 20, 1000)
+        if not hasattr(self, '_f_light_3d') or new_compute is True:
+            r_array = np.logspace(np.log10(self._min_interpolate), np.log10(self._max_interpolate), self._interp_grid_num)
             light_3d_array = self.light_model.light_3d(r_array, kwargs_list)
-            light_3d_array[light_3d_array < 10**(-10)] = 10**(-10)
             f = interp1d(np.log(r_array), np.log(light_3d_array), fill_value="extrapolate")
             self._f_light_3d = f
         return np.exp(self._f_light_3d(np.log(r)))
@@ -61,10 +63,36 @@ class LightProfile(object):
         :param kwargs_light:
         :return:
         """
-        r_array = np.linspace(0.0001, 20, 1000)
-        dr = r_array[1] - r_array[0]
+        min_log = np.log10(self._min_interpolate)
+        max_log = np.log10(self._max_interpolate)
+        num = self._interp_grid_num
+        r_array = np.logspace(min_log, max_log, num)
+        dlogr = float(max_log - min_log) / num * np.log(10)
         r_ = np.sqrt(r_array**2 + R**2)
-        return np.sum(self.light_3d_interp(r_, kwargs_light)) * 2 * dr
+        return np.sum(self.light_3d_interp(r_, kwargs_light) * r_array) * 2 * dlogr
+
+    def draw_light_2d_linear(self, kwargs_list, n=1, new_compute=False, r_eff=1.):
+        """
+        constructs the CDF and draws from it random realizations of projected radii R
+        :param kwargs_list:
+        :return:
+        """
+        if not hasattr(self, '_light_cdf') or new_compute is True:
+            r_array = np.linspace(0., self._max_interpolate, self._interp_grid_num)
+            cum_sum = np.zeros_like(r_array)
+            sum = 0
+            for i, r in enumerate(r_array):
+                if i == 0:
+                    cum_sum[i] = 0
+                else:
+                    sum += self.light_2d(r, kwargs_list) * r
+                    cum_sum[i] = copy.deepcopy(sum)
+            cum_sum_norm = cum_sum/cum_sum[-1]
+            f = interp1d(cum_sum_norm, r_array)
+            self._light_cdf = f
+        cdf_draw = np.random.uniform(0., 1, n)
+        r_draw = self._light_cdf(cdf_draw)
+        return r_draw
 
     def draw_light_2d(self, kwargs_list, n=1, new_compute=False, r_eff=1.):
         """
@@ -72,19 +100,22 @@ class LightProfile(object):
         :param kwargs_list:
         :return:
         """
-        if not hasattr(self, '_light_cdf') or new_compute is True:
-            r_array = np.linspace(0.000001, 10*r_eff, 1000)
+        if not hasattr(self, '_light_cdf_log') or new_compute is True:
+            r_array = np.logspace(np.log10(self._min_interpolate), np.log10(self._max_interpolate), self._interp_grid_num)
             cum_sum = np.zeros_like(r_array)
             sum = 0
             for i, r in enumerate(r_array):
-                sum += self.light_2d(r, kwargs_list) * r
-                cum_sum[i] = copy.deepcopy(sum)
+                if i == 0:
+                    cum_sum[i] = 0
+                else:
+                    sum += self.light_2d(r, kwargs_list) * r * r
+                    cum_sum[i] = copy.deepcopy(sum)
             cum_sum_norm = cum_sum/cum_sum[-1]
-            f = interp1d(cum_sum_norm, r_array)
-            self._light_cdf = f
-        cdf_draw = np.random.uniform(0.000001, 1, n)
-        r_draw = self._light_cdf(cdf_draw)
-        return r_draw
+            f = interp1d(cum_sum_norm, np.log(r_array))
+            self._light_cdf_log = f
+        cdf_draw = np.random.uniform(0., 1, n)
+        r_log_draw = self._light_cdf_log(cdf_draw)
+        return np.exp(r_log_draw)
 
 
 class LightProfile_old(object):

galkin/mass_profile.py
@@ -9,7 +9,7 @@ class MassProfile(object):
     """
     mass profile class
     """
-    def __init__(self, profile_list, kwargs_cosmo={'D_d': 1000, 'D_s': 2000, 'D_ds': 500}):
+    def __init__(self, profile_list, kwargs_cosmo={'D_d': 1000, 'D_s': 2000, 'D_ds': 500}, kwargs_numerics={}):
         """
 
         :param profile_list:
@@ -17,6 +17,9 @@ class MassProfile(object):
         kwargs_options = {'lens_model_list': profile_list}
         self.model = LensModel(kwargs_options)
         self.cosmo = Cosmo(kwargs_cosmo)
+        self._interp_grid_num = kwargs_numerics.get('interpol_grid_num', 1000)
+        self._max_interpolate = kwargs_numerics.get('max_interpolate', 100)
+        self._min_interpolate = kwargs_numerics.get('min_interpolate', 0.0001)
 
     def mass_3d_interp(self, r, kwargs, new_compute=False):
         """
@@ -26,13 +29,13 @@ class MassProfile(object):
         :return: mass enclosed physical radius in kg
         """
         if not hasattr(self, '_log_mass_3d') or new_compute is True:
-            r_array = np.linspace(0.0001, 20, 1000)
+            r_array = np.logspace(np.log10(self._min_interpolate), np.log10(self._max_interpolate), self._interp_grid_num)
             mass_3d_array = self.model.mass_3d(r_array, kwargs)
             mass_dim_array = mass_3d_array * const.arcsec ** 3 * self.cosmo.D_d ** 2 * self.cosmo.D_s \
                        / self.cosmo.D_ds * const.Mpc * const.c ** 2 / (4 * np.pi * const.G)
-            f = interp1d(r_array, np.log(mass_dim_array/r_array), fill_value="extrapolate")
+            f = interp1d(np.log(r_array), np.log(mass_dim_array/r_array), fill_value="extrapolate")
             self._log_mass_3d = f
-        return np.exp(self._log_mass_3d(r)) * r
+        return np.exp(self._log_mass_3d(np.log(r))) * r
 
     def mass_3d(self, r, kwargs):
         """

test/test_galkin.py
@@ -16,9 +16,6 @@ class TestGalkin(object):
     def setup(self):
         pass
 
-    def test_test(self):
-        assert 0 == 0
-
     def test_galkin_vs_LOS_dispersion(self):
         """
         tests whether the old and new version provide the same answer
@@ -52,21 +49,62 @@ class TestGalkin(object):
         width = 0.9
         kwargs_aperture = {'length': length, 'width': width, 'center_ra': 0, 'center_dec': 0, 'angle': 0}
 
-        psf_fwhm = 0.7  # Gaussian FWHM psf
+        psf_fwhm = 0.1  # Gaussian FWHM psf
         kwargs_cosmo = {'D_d': 1000, 'D_s': 1500, 'D_ds': 800}
+
         galkin = GalKin_old(aperture=aperture_type, mass_profile=mass_profile, light_profile=light_profile,
                             anisotropy_type=anisotropy_type, psf_fwhm=psf_fwhm, kwargs_cosmo=kwargs_cosmo)
-        sigma_v = galkin.vel_disp(kwargs_profile, kwargs_aperture, kwargs_light, kwargs_anisotropy, num=100)
+        sigma_v = galkin.vel_disp(kwargs_profile, kwargs_aperture, kwargs_light, kwargs_anisotropy, num=1000)
 
         los_disp = Velocity_dispersion(beta_const=False, b_prior=False, kwargs_cosmo=kwargs_cosmo)
-        sigma_v2 = los_disp.vel_disp(gamma, theta_E, r_eff, aniso_param=r_ani, R_slit=length/2., dR_slit=width/2.,
-                                     FWHM=psf_fwhm, num=100)
-        npt.assert_almost_equal((sigma_v-sigma_v2)/sigma_v2, 0, decimal=1)
+        sigma_v2 = los_disp.vel_disp(gamma, theta_E, r_eff, aniso_param=r_ani, R_slit=length, dR_slit=width,
+                                     FWHM=psf_fwhm, num=1000)
+        npt.assert_almost_equal((sigma_v-sigma_v2)/sigma_v2, 0, decimal=2)
+
+    def test_log_linear_integral(self):
+        # light profile
+        light_profile_list = ['HERNQUIST']
+        r_eff = .5
+        kwargs_light = [{'Rs':  r_eff, 'sigma0': 1.}]  # effective half light radius (2d projected) in arcsec
+        # 0.551 *
+        # mass profile
+        mass_profile_list = ['SPP']
+        theta_E = 1.2
+        gamma = 2.
+        kwargs_profile = [{'theta_E': theta_E, 'gamma': gamma}]  # Einstein radius (arcsec) and power-law slope
+
+        # anisotropy profile
+        anisotropy_type = 'OsipkovMerritt'
+        r_ani = 2.
+        kwargs_anisotropy = {'r_ani': r_ani}  # anisotropy radius [arcsec]
+
+        # aperture as slit
+        aperture_type = 'slit'
+
+        psf_fwhm = 0.7  # Gaussian FWHM psf
+        kwargs_cosmo = {'D_d': 1000, 'D_s': 1500, 'D_ds': 800}
+        kwargs_numerics_linear = {'sampling_number': 5000, 'interpol_grid_num': 5000, 'log_integration': False,
+                           'max_integrate': 50}
+        kwargs_numerics_log = {'sampling_number': 5000, 'interpol_grid_num': 5000, 'log_integration': True,
+                           'max_integrate': 50}
+        galkin_linear = Galkin(mass_profile_list, light_profile_list, aperture_type=aperture_type,
+                        anisotropy_model=anisotropy_type, fwhm=psf_fwhm, kwargs_cosmo=kwargs_cosmo, kwargs_numerics=kwargs_numerics_linear)
+        galkin_log = Galkin(mass_profile_list, light_profile_list, aperture_type=aperture_type,
+                        anisotropy_model=anisotropy_type, fwhm=psf_fwhm, kwargs_cosmo=kwargs_cosmo, kwargs_numerics=kwargs_numerics_log)
+        R = np.linspace(0.05, 1, 100)
+        lin_I_R = np.zeros_like(R)
+        log_I_R = np.zeros_like(R)
+        for i in range(len(R)):
+            lin_I_R[i] = galkin_linear.I_R_simga2(R[i], kwargs_profile, kwargs_light, kwargs_anisotropy)
+            log_I_R[i] = galkin_log.I_R_simga2(R[i], kwargs_profile, kwargs_light, kwargs_anisotropy)
+        print(log_I_R/lin_I_R)
+        for i in range(len(R)):
+            npt.assert_almost_equal(log_I_R[i] / lin_I_R[i], 1, decimal=2)
 
     def test_log_vs_linear_integral(self):
         # light profile
         light_profile_list = ['HERNQUIST']
-        r_eff = 1.8
+        r_eff = .5
         kwargs_light = [{'Rs':  r_eff, 'sigma0': 1.}]  # effective half light radius (2d projected) in arcsec
         # 0.551 *
         # mass profile
@@ -88,13 +126,20 @@ class TestGalkin(object):
 
         psf_fwhm = 0.7  # Gaussian FWHM psf
         kwargs_cosmo = {'D_d': 1000, 'D_s': 1500, 'D_ds': 800}
-        galkin = Galkin(mass_profile_list, light_profile_list, aperture_type=aperture_type, anisotropy_model=anisotropy_type, fwhm=psf_fwhm, kwargs_cosmo=kwargs_cosmo)
-        sigma_v = galkin.vel_disp(kwargs_profile, kwargs_light, kwargs_anisotropy, kwargs_aperture, num=1000)
-        sigma_v2 = galkin.vel_disp(kwargs_profile, kwargs_light, kwargs_anisotropy, kwargs_aperture, num=1000, log_int=True)
+        kwargs_numerics_log = {'sampling_number': 5000, 'interpol_grid_num': 5000, 'log_integration': True,
+                           'max_integrate': 100}
+        kwargs_numerics_linear = {'sampling_number': 5000, 'interpol_grid_num': 5000, 'log_integration': False,
+                           'max_integrate': 100}
+        galkin_linear = Galkin(mass_profile_list, light_profile_list, aperture_type=aperture_type, anisotropy_model=anisotropy_type, fwhm=psf_fwhm, kwargs_cosmo=kwargs_cosmo, kwargs_numerics=kwargs_numerics_linear)
+
+        sigma_v = galkin_linear.vel_disp(kwargs_profile, kwargs_light, kwargs_anisotropy, kwargs_aperture)
+        galkin_log = Galkin(mass_profile_list, light_profile_list, aperture_type=aperture_type,
+                        anisotropy_model=anisotropy_type, fwhm=psf_fwhm, kwargs_cosmo=kwargs_cosmo, kwargs_numerics=kwargs_numerics_log)
+        sigma_v2 = galkin_log.vel_disp(kwargs_profile, kwargs_light, kwargs_anisotropy, kwargs_aperture)
         print sigma_v, sigma_v2, 'sigma_v linear, sigma_v log'
         print (sigma_v/sigma_v2)**2
 
-        npt.assert_almost_equal((sigma_v-sigma_v2)/sigma_v2, 0, decimal=1)
+        npt.assert_almost_equal(sigma_v/sigma_v2, 1, decimal=2)
 
     def test_compare_power_law(self):
         """
@@ -103,7 +148,7 @@ class TestGalkin(object):
         """
         # light profile
         light_profile_list = ['HERNQUIST']
-        r_eff = 1.8
+        r_eff = .5
         kwargs_light = [{'Rs':  r_eff, 'sigma0': 1.}]  # effective half light radius (2d projected) in arcsec
         # 0.551 *
         # mass profile
@@ -119,36 +164,22 @@ class TestGalkin(object):
 
         # aperture as slit
         aperture_type = 'slit'
-        length = 3.8
-        width = 0.9
+        length = 1.
+        width = 0.3
         kwargs_aperture = {'length': length, 'width': width, 'center_ra': 0, 'center_dec': 0, 'angle': 0}
 
-        psf_fwhm = 0.7  # Gaussian FWHM psf
+        psf_fwhm = 1.  # Gaussian FWHM psf
         kwargs_cosmo = {'D_d': 1000, 'D_s': 1500, 'D_ds': 800}
-        galkin = Galkin(mass_profile_list, light_profile_list, aperture_type=aperture_type, anisotropy_model=anisotropy_type, fwhm=psf_fwhm, kwargs_cosmo=kwargs_cosmo)
-        sigma_v = galkin.vel_disp(kwargs_profile, kwargs_light, kwargs_anisotropy, kwargs_aperture, num=1000, log_int=True)
+        kwargs_numerics = {'sampling_number': 5000, 'interpol_grid_num': 5000, 'log_integration': False,
+                           'max_integrate': 500}
+        galkin = Galkin(mass_profile_list, light_profile_list, aperture_type=aperture_type, anisotropy_model=anisotropy_type, fwhm=psf_fwhm, kwargs_cosmo=kwargs_cosmo, kwargs_numerics=kwargs_numerics)
+        sigma_v = galkin.vel_disp(kwargs_profile, kwargs_light, kwargs_anisotropy, kwargs_aperture)
 
         los_disp = Velocity_dispersion(beta_const=False, b_prior=False, kwargs_cosmo=kwargs_cosmo)
-        sigma_v2 = los_disp.vel_disp(gamma, theta_E, r_eff, aniso_param=r_ani, R_slit=length/2., dR_slit=width/2.,
-                                     FWHM=psf_fwhm, num=1000)
+        sigma_v2 = los_disp.vel_disp(gamma, theta_E, r_eff, aniso_param=r_ani, R_slit=length, dR_slit=width,
+                                     FWHM=psf_fwhm, num=5000)
         print sigma_v, sigma_v2, 'sigma_v Galkin, sigma_v los dispersion'
-        print (sigma_v/sigma_v2)**2
-
-        npt.assert_almost_equal((sigma_v-sigma_v2)/sigma_v2, 0, decimal=1)
-
-    def test_numeric_light_integral(self):
-        """
-
-        :return:
-        """
-        light_profile_list = ['HERNQUIST']
-        r_eff = .05
-        kwargs_light = [{'Rs': r_eff, 'sigma0': 1.}]  # effective half light radius (2d projected) in arcsec
-        lightProfile = LightProfile(light_profile_list)
-        R = 2
-        light2d = lightProfile.light_2d(R=R, kwargs_list=kwargs_light)
-        light2d_int = lightProfile._integrand_light(R, kwargs_light)
-        npt.assert_almost_equal(light2d/light2d_int, 1, decimal=2)
+        npt.assert_almost_equal(sigma_v2/sigma_v, 1, decimal=2)
 
     def test_projected_light_integral_hernquist(self):
         """
@@ -161,7 +192,7 @@ class TestGalkin(object):
         lightProfile = LightProfile(light_profile_list)
         R = 2
         light2d = lightProfile.light_2d(R=R, kwargs_list=kwargs_light)
-        out = integrate.quad(lambda x: lightProfile.light_3d(np.sqrt(R**2+x**2), kwargs_light), 0, 10)
+        out = integrate.quad(lambda x: lightProfile.light_3d(np.sqrt(R**2+x**2), kwargs_light), 0, 100)
         npt.assert_almost_equal(light2d, out[0]*2, decimal=3)
 
     def test_projected_light_integral_hernquist_ellipse(self):

test/test_light_profile.py
@@ -15,31 +15,43 @@ class TestLightProfile(object):
 
     def test_draw_light(self):
         lightProfile = LightProfile(profile_list=['HERNQUIST'])
-        kwargs_profile = [{'sigma0': 1., 'Rs': 0.5}]
-        r_list = lightProfile.draw_light_2d(kwargs_profile, n=10000)
-        npt.assert_almost_equal(np.median(r_list), 0.8, decimal=1)
+        kwargs_profile = [{'sigma0': 1., 'Rs': 0.8}]
+        r_list = lightProfile.draw_light_2d(kwargs_profile, n=100000)
+        bins = np.linspace(0., 1, 20)
+        hist, bins_hist = np.histogram(r_list, bins=bins, normed=True)
+        light2d = lightProfile.light_2d(R=(bins_hist[1:] + bins_hist[:-1])/2., kwargs_list=kwargs_profile)
+        light2d *= (bins_hist[1:] + bins_hist[:-1]) / 2.
+        light2d /= np.sum(light2d)
+        hist /= np.sum(hist)
+        print light2d / hist
+        for i in range(len(hist)):
+            print bins_hist[i], i, light2d[i] / hist[i]
+            npt.assert_almost_equal(light2d[i] / hist[i], 1, decimal=1)
 
     def test_draw_light_PJaffe(self):
         lightProfile = LightProfile(profile_list=['PJAFFE'])
         kwargs_profile = [{'sigma0': 1., 'Rs': 0.5, 'Ra': 0.2}]
-        r_list = lightProfile.draw_light_2d(kwargs_profile, n=10000)
-        bins = np.linspace(0, 10, 100)
+        r_list = lightProfile.draw_light_2d(kwargs_profile, n=50000)
+        bins = np.linspace(0, 1, 10)
         hist, bins_hist = np.histogram(r_list, bins=bins, normed=True)
-        light2d = lightProfile.light_2d(R=bins_hist[1:], kwargs_list=kwargs_profile)
-        light2d *= bins_hist[1:]
+        light2d = lightProfile.light_2d(R=(bins_hist[1:] + bins_hist[:-1])/2., kwargs_list=kwargs_profile)
+        light2d *= (bins_hist[1:] + bins_hist[:-1]) / 2.
         light2d /= np.sum(light2d)
         hist /= np.sum(hist)
-        npt.assert_almost_equal(light2d[1], hist[1], decimal=2)
+        #print light2d / hist
+        npt.assert_almost_equal(light2d[4]/hist[4], 1, decimal=2)
 
+        lightProfile = LightProfile(profile_list=['PJAFFE'], kwargs_numerics={'min_interpolate': 0.0001, 'max_interpolate': 20.})
         kwargs_profile = [{'sigma0': 1., 'Rs': 0.04, 'Ra': 0.02}]
-        r_list = lightProfile.draw_light_2d(kwargs_profile, n=10000)
-        bins = np.linspace(0, 1, 1000)
+        r_list = lightProfile.draw_light_2d(kwargs_profile, n=50000)
+        bins = np.linspace(0., 0.1, 10)
         hist, bins_hist = np.histogram(r_list, bins=bins, normed=True)
-        light2d = lightProfile.light_2d(R=bins_hist[1:], kwargs_list=kwargs_profile)
-        light2d *= bins_hist[1:]
+        light2d = lightProfile.light_2d(R=(bins_hist[1:] + bins_hist[:-1])/2., kwargs_list=kwargs_profile)
+        light2d *= (bins_hist[1:] + bins_hist[:-1]) / 2.
         light2d /= np.sum(light2d)
         hist /= np.sum(hist)
-        npt.assert_almost_equal(light2d[1], hist[1], decimal=2)
+        print light2d / hist
+        npt.assert_almost_equal(light2d[4] / hist[4], 1, decimal=2)
 
     def test_ellipticity_in_profiles(self):
         lightProfile = ['HERNQUIST_ELLIPSE', 'PJAFFE_ELLIPSE']
@@ -54,15 +66,17 @@ class TestLightProfile(object):
         kwargs_profile[0]['q'] = 1
         kwargs_profile[1]['q'] = 1
         r_eff_spherical = lensAnalysis.half_light_radius(kwargs_profile)
-        npt.assert_almost_equal(r_eff, r_eff_spherical, decimal=2)
+        npt.assert_almost_equal(r_eff / r_eff_spherical, 1, decimal=2)
 
     def test_light_3d(self):
         lightProfile = LightProfile(profile_list=['HERNQUIST'])
-        r = 0.3
+        r = np.logspace(-2,2, 100)
         kwargs_profile = [{'sigma0': 1., 'Rs': 0.5}]
         light_3d = lightProfile.light_3d_interp(r, kwargs_profile)
         light_3d_exact = lightProfile.light_3d(r, kwargs_profile)
-        npt.assert_almost_equal(light_3d/light_3d_exact, 1, decimal=3)
+        for i in range(len(r)):
+            npt.assert_almost_equal(light_3d[i]/light_3d_exact[i], 1, decimal=3)
+
 
 if __name__ == '__main__':
     pytest.main()